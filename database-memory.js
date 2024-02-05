7cf89945a283dff9e70f5d8e45467871d062f1ec
Guilherme Gama <guilhermegamabs@gmail.com>
2024-02-05 17:38:22 -0300

    CRUD
    CRUD usando banco de dados em memória

--- database-memory.js
+++ database-memory.js
@@ -4,7 +4,16 @@ export class DatabaseMemory {
     #videos = new Map();
 
     list() {
-        return this.#videos.values();
+        // esse método from faz com que algo se torne um Array
+        return Array.from(this.#videos.entries()).map((videoArray) => {
+            const id = videoArray[0];
+            const data = videoArray[1];
+
+            return {
+                id,
+                ...data
+            }
+        });
     }
 
     create(video) {
--- server.js
+++ server.js
@@ -1,24 +1,49 @@
 import { fastify } from 'fastify';
-import { DatabaseMemory } from './database-memory';
+import { DatabaseMemory } from './database-memory.js';
 
 const server = fastify();
 const PORT = 3333;
 
-server.get("/", () => {
-    return 'Hello World!';
+const database = new DatabaseMemory();
+
+server.get("/videos", (req, reply) => {
+    const videos = database.list();
+
+    return videos;
 });
 
-server.post('/videos', () => {
-    return 'Hello';
+server.post('/videos', (req, reply) => {
+    const { title, description, duration } = req.body;
+
+    database.create({
+        title,
+        description,
+        duration,
+    });
+
+    return  reply.status(201).send();
 });
 
-server.put('/videos/:id', () => {
-    return 'Hello Node.js';
+server.put('/videos/:id', (req, reply) => {
+    const videoID = req.params.id;
+    const { title, description, duration } = req.body;
+    
+    const video = database.update(videoID, {
+        title,
+        description,
+        duration
+    });
+
+    return reply.status(204).send();
 });
 
-server.delete('/videos/:id', () => {
-    return 'Hello Node.js';
-})
+server.delete('/videos/:id', (req, reply) => {
+    const videoID = req.params.id;
+    
+    database.delete(videoID);
+
+    return reply.status(200).send();
+});
 
 server.listen({
     port: PORT,
